4250304d7048068d3fb3ccc274faa913109fce8d
root <root@ikensc1.eng.platformlab.ibm.com>
2016-02-26 16:32:45 -0500

    add basic message handling

--- wechatsimi.py
+++ wechatsimi.py
@@ -3,6 +3,9 @@ import ssl
 import re
 import time
 import os
+import xml.dom.minidom
+import json
+import requests
 try:
     from urllib import urlencode, quote_plus
     from cookielib import CookieJar
@@ -16,11 +19,22 @@ except ImportError:
     import urllib.request as urllib
 
 
-uuid = "" #declare uuid as a global variable
+uuid = '' #declare uuid as a global variable
 tip = 1 #whether the qr is scanned(0) or not(1)
-baseuri = ""
-redirecturi = ""
-push_uri = ""
+baseuri = ''
+redirecturi = ''
+push_uri = ''
+
+skey = ''
+wxsid = ''
+wxuin = ''
+pass_ticket = ''
+deviceId = 'e000000000000000'
+
+BaseRequest = {}
+ContactList = []
+My = []
+SyncKey = []
 
 QRImagePath = os.path.join(os.getcwd(), 'qrcode'+str(time.time())+'.jpg')
     
@@ -29,10 +43,118 @@ def configure_logger():
     logging.basicConfig(format=FORMAT, level=logging.DEBUG, filename='WechatSimi.log')
     logger = logging.getLogger()
 
+
+def syncKey():
+    SyncKeyItems = ['%s_%s' % (item['Key'], item['Val'])
+                    for item in SyncKey['List']] #SyncKey is set in initWebWechat
+    SyncKeyStr = '|'.join(SyncKeyItems)
+    return SyncKeyStr
+    
+def syncCheck():
+    url = push_uri + '/synccheck?'
+    params = {
+        'skey': BaseRequest['Skey'],
+        'sid': BaseRequest['Sid'],
+        'uin': BaseRequest['Uin'],
+        'deviceId': BaseRequest['DeviceID'],
+        'synckey': syncKey(),
+        'r': int(time.time()),
+    }
+    request = getRequest(url=url + urlencode(params))
+    response = urllib.urlopen(request)
+    data = response.read().decode('utf-8', 'replace')
+    logging.debug("Data received in syncCheck: " + data)
+    
+    regx = r'window.synccheck={retcode:"(\d+)",selector:"(\d+)"}'
+    match = re.search(regx, data)
+
+    retcode = match.group(1)
+    selector = match.group(2)
+    
+    return selector
+    
+def syncWebWechat():
+    global SyncKey
+    url = base_uri + '/webwxsync?lang=zh_CN&skey=%s&sid=%s&pass_ticket=%s' % (
+        BaseRequest['Skey'], BaseRequest['Sid'], quote_plus(pass_ticket))
+    params = {
+        'BaseRequest': BaseRequest,
+        'SyncKey': SyncKey,
+        'rr': ~int(time.time()),
+    }
+    request = getRequest(url=url, data=json.dumps(params))
+    request.add_header('ContentType', 'application/json; charset=UTF-8')
+    response = urllib.urlopen(request)
+    data = response.read().decode('utf-8', 'replace')
+    logging.debug("Data received in syncWebWechat: " + data)
+    
+    dict = json.loads(data)
+    SyncKey = dict['SyncKey']
+
+    ErrMsg = dict['BaseResponse']['ErrMsg']
+    Ret = dict['BaseResponse']['Ret']
+    logging.debug('synWebWechat: Ret: %d, ErrMsg: %s' % (Ret, ErrMsg))
+
+    return dict
+    
 def heartBeatLoop():
     logging.info('Start heart beat loop...')
     while True:
-        pass
+        selector = syncCheck()
+        if selector == '2': #New Message
+            result = syncWebWechat()
+            if result:
+                handleMessage(result)
+        else:
+            time.sleep(1)
+            
+
+def getUserRemarkName(self, id):
+        return "test"
+        
+        name = 'Unknown group' if id[:2] == '@@' else 'Stranger'
+        
+        if id == My['UserName']: return My['NickName']  #Self
+
+        if id[:2] == '@@':
+            # group
+            name = getGroupName(id) 
+        else:
+            for member in SpecialUsersList:
+                if member['UserName'] == id:
+                    name = member['RemarkName'] if member['RemarkName'] else member['NickName']
+            # Public User
+            for member in PublicUsersList:
+                if member['UserName'] == id:
+                    name = member['RemarkName'] if member['RemarkName'] else member['NickName']
+
+            # Contact List
+            for member in self.ContactList:
+                if member['UserName'] == id:
+                    name = member['RemarkName'] if member['RemarkName'] else member['NickName']
+            # Group Member List
+            for member in self.GroupMemeberList:
+                if member['UserName'] == id:
+                    name = member['DisplayName'] if member['DisplayName'] else member['NickName']
+
+        if name == 'Unknown group' or name == 'Stranger': 
+            logging.debug("Cannot resolve the following id: " + id)
+            
+        return name
+            
+def handleMessage(messageDict):
+    logging.debug('handleMessage is processing the following dictionary: ' + str(messageDict))
+    for msg in r['AddMsgList']:
+        print ('New Message Received!')
+        msgType = msg['MsgType']
+        name = getUserRemarkName(msg['FromUserName'])
+        content =  msg['Content'].replace('&lt;','<').replace('&gt;','>')
+        msgId = msg['MsgId']
+        
+        if msgType == 1:
+            raw_msg = {'raw_msg': msg}
+            print ('Received msg %s from user %s' % (raw_msg, name))
+    
 
 def getRequest(url, data=None):
     try:
@@ -42,6 +164,8 @@ def getRequest(url, data=None):
     finally:
         return urllib.Request(url=url, data=data)
         
+
+    
 def getUUID():
     global uuid
     
@@ -93,7 +217,7 @@ def showQRCode():
     
     try:
         with open(QRImagePath, 'wb') as fp:
-            f.write(response.read())
+            fp.write(response.read())
         logging.info('QRImage is saved at ' + QRImagePath)
         print ('Please scan QR code to login: ' + QRImagePath)
     except Exception as e:
@@ -118,7 +242,7 @@ def waitForLogin():
         logging.debug("Return code is " + str(code))
         
         if code == '201': #Scanned
-            print('Succeed! Please confirm on your phone to login')
+            print('Success! Please confirm on your phone to login')
             tip = 0
         elif code == '200': #Logged in
             print('Logging in...')
@@ -140,16 +264,16 @@ def waitForLogin():
             for (searchUrl, pushUrl) in services:
                 if base_uri.find(searchUrl) >= 0:
                     push_uri = 'https://%s/cgi-bin/mmwebwx-bin' % pushUrl
-                    break
+                    break            
         elif code == '408': #Timeout
             print ('Timeout, during login...')
-
+            
         return code
     
 def loginWechat():
     global skey, wxsid, wxuin, pass_ticket, BaseRequest
     request = getRequest(url=redirect_uri)
-    response = wdf_urllib.urlopen(request)
+    response = urllib.urlopen(request)
     data = response.read().decode('utf-8', 'replace')
     logging.debug('Data retrieved from login request: ' + str(data))
     
@@ -183,6 +307,8 @@ def loginWechat():
     
     
 def initWebWechat():
+    print ('Initializing Web Wechat...')
+    logging.info('Start to init web wechat')
     url = base_uri + \
         '/webwxinit?pass_ticket=%s&skey=%s&r=%s' % (
             pass_ticket, skey, int(time.time())
@@ -193,19 +319,38 @@ def initWebWechat():
 
     request = getRequest(url=url, data=json.dumps(params))
     request.add_header('ContentType', 'application/json; charset=UTF-8')
-    response = wdf_urllib.urlopen(request)
+    response = urllib.urlopen(request)
     data = response.read()
     data = data.decode('utf-8', 'replace')
-    logging.debug('Data retrieved from login request: ' + str(data))
+    dict = json.loads(data)
+    logging.debug('Data retrieved from initWebWechat request: ' + str(dict))
     
     global ContactList, My, SyncKey
-    dict = json.loads(data)
     ContactList = dict['ContactList']
     My = dict['User']
     SyncKey = dict['SyncKey']
 
-    state = responseState('webwxinit', dict['BaseResponse'])
+    ErrMsg = dict['BaseResponse']['ErrMsg']
+    Ret = dict['BaseResponse']['Ret']
+    logging.debug('synWebWechat: Ret: %d, ErrMsg: %s' % (Ret, ErrMsg))
+
+    state = (Ret == 0)
     return state
+
+def getSimiReply(text):
+    key="89406ec6-3acb-4e04-ae7f-f00b32289e9f" #Valid for 7 days
+    url='http://sandbox.api.simsimi.com/request.p?key=%s&lc=ch&ft=0.0&text=%s'%(key, text)
+    try:
+        r=requests.get(url)
+        data = r.json()
+        logging.debug("Return result for text "+text+" is " + data)
+        if data['result'] == 100:
+            return data['response']
+        else:
+            return "Hehe, I need to go take a shower now"
+    except Exception as e:
+        loggine.error("Exception occurs during simi reply " + e)
+        return "That's interesting"
     
 def main():
     configure_logger()
@@ -239,8 +384,15 @@ def main():
     if not initWebWechat():
         logging.error("Initializing Web Wechat failed...")
         return
-        
-    heartBeatLoop()
+    
+    print('Start heart beat loop')
+    thread.start_new_thread(heartBeatLoop, ())
+    
+    MemberList = getContactFromWebWechat()
+    print('You have %s friends in contact list' % len(MemberList))
+    
+    
+    
     
 
 if __name__ == '__main__':
